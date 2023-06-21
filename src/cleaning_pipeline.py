85801ab83eb3b3f9cee7c5927f539278f3781f1f
Jamison Kelly <jamison.m.kelly@gmail.com>
2023-06-21 14:56:17 -0700

    created cleaning pipeline py file to house data parsing pipelines.

---
+++ src/cleaning_pipeline.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import scipy.stats as stats 
+import seaborn as sns 
+import sklearn as skl 
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn import metrics
+import math 
+from pandas.plotting import scatter_matrix
